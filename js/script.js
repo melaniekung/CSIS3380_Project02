eaf6af29c91d42fee1f89601e8a0a13d85abd56a
melaniekung <kungm1@student.douglascollege.ca>
2022-03-09 15:53:31 -0800

    Update code to calculate pages

--- js/script.js
+++ js/script.js
@@ -1,18 +1,27 @@
-// contactlist defined to use to change innerHTML
+// get required elements
 var contactlist = document.getElementsByClassName('contact-list');
+var pages = document.getElementsByClassName('pagination');
 // create contacts array obtained using DOM
 const contacts = Array.from(document.getElementsByClassName('contact-item cf'));
 
-// function to display corresponding contacts to page
 function paginate(page) {
+    // get html for pages
+    let pagehtml = "";
+
+    for (var i = 0; i <= contacts.length % 10; i++) {
+        pagehtml += "<li><a onClick='paginate("  + (i + 1) + ")' href='#'>" + (i + 1) + "</a></li>";
+    }
+
+    pages[0].innerHTML = pagehtml;
+
     // start an empty string to collect html to add to contactlist
-    let html = "";
+    let contacthtml = "";
 
     // iterate through contact list to collect the required html for display
     for (var i = (page * 10) - 10; i <= (page * 10) - 1; i++) {
-        html += (contacts[i]).outerHTML;
+        contacthtml += (contacts[i]).outerHTML;
     }
 
     // load html to class element "contact-list"
-    contactlist[0].innerHTML = html;
+    contactlist[0].innerHTML = contacthtml;
 }
